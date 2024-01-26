589d26767fa8066ad4d3c399833dc39dc580a089
Shalom-302 <54105227+Shalom-302@users.noreply.github.com>
2024-01-26 17:52:19 +0000

    Update main.py

--- main.py
+++ main.py
@@ -35,14 +35,14 @@ def get_text_chunks(text):
 
 
 def get_vector_store(text_chunks):
-    embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
+    embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001", google_api_key=gemini_api_key)
     vector_store = FAISS.from_texts(text_chunks, embedding=embeddings)
-    vector_store.save_local("faiss_index")
+    return vector_store
 
 
 def get_conversational_chain():
     prompt_template = """
-    Answer the question as detailed as possible from the provided context, make sure to provide all the details, 
+    Answer the question as detailed as possible from the provided context, make sure to provide all the details,
     if the answer is not in
     provided context just say, "answer is not available in the context", don't provide the wrong answer\n\n
     Context:\n {context}?\n
@@ -61,7 +61,7 @@ def get_conversational_chain():
 
 
 def user_input(user_question):
-    embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
+    embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001", google_api_key=gemini_api_key)
 
     new_db = FAISS.load_local("faiss_index", embeddings)
     docs = new_db.similarity_search(user_question)
@@ -70,7 +70,7 @@ def user_input(user_question):
 
     response = chain(
         {"input_documents": docs, "question": user_question}
-        , return_only_outputs=True)
+    )
 
     print(response)
     st.write("Reply: ", response["output_text"])
@@ -88,7 +88,7 @@ def main():
     with st.sidebar:
         st.title("Menu:")
         pdf_docs = st.file_uploader("Téléchargez vos fichiers PDF et cliquez sur le bouton Soumettre et traiter",
-                                    accept_multiple_files=True)
+                                     accept_multiple_files=True)
         if st.button("Soumettre et traiter"):
             with st.spinner("Traitement..."):
                 raw_text = get_pdf_text(pdf_docs)
