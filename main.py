94afa4cb8b6f1405e8c984e6d3c69fd15fd63086
Shalom-302 <54105227+Shalom-302@users.noreply.github.com>
2024-10-02 14:17:21 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
+
 import streamlit as st
 from PyPDF2 import PdfReader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 import os
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
 import google.generativeai as genai
-from langchain.vectorstores import FAISS
+from langchain_community.vectorstores import FAISS
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.chains.question_answering import load_qa_chain
 from langchain.prompts import PromptTemplate
@@ -13,9 +14,6 @@ from dotenv import load_dotenv
 load_dotenv()
 os.getenv("GOOGLE_API_KEY")
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
-
-
-
 
 
 
@@ -42,17 +40,19 @@ def get_vector_store(text_chunks):
 
 
 def get_conversational_chain():
-
+    
     prompt_template = """
-    Answer the question as detailed as possible from the provided context, make sure to provide all the details, if the answer is not in
-    provided context just say, "answer is not available in the context", don't provide the wrong answer\n\n
-    Context:\n {context}?\n
-    Question: \n{question}\n
+    Répondez à la question de la manière la plus détaillée possible à partir du document PDF fourni. Assurez-vous de fournir tous les détails pertinents. Si la réponse ne peut pas être déterminée à partir du document PDF, dites simplement "la réponse n'est pas disponible dans le PDF", sans fournir de réponse incorrecte.
 
-    Answer:
+    Document PDF fourni : {context}
+    \n
+    Question : \n{question}\n
+
+    Réponse :
     """
 
-    model = ChatGoogleGenerativeAI(model="gemini-pro",
+
+    model = ChatGoogleGenerativeAI(model="gemini-1.5-pro",
                              temperature=0.3)
 
     prompt = PromptTemplate(template = prompt_template, input_variables = ["context", "question"])
@@ -61,19 +61,17 @@ def get_conversational_chain():
     return chain
 
 
-
 def user_input(user_question):
     embeddings = GoogleGenerativeAIEmbeddings(model = "models/embedding-001")
+    new_db = FAISS.load_local("faiss_index", embeddings, allow_dangerous_deserialization=True)
     
-    new_db = FAISS.load_local("faiss_index", embeddings)
     docs = new_db.similarity_search(user_question)
-
-    chain = get_conversational_chain()
-
     
-    response = chain(
-        {"input_documents":docs, "question": user_question}
-        , return_only_outputs=True)
+    conversational_chain = get_conversational_chain()
+
+    response = conversational_chain.invoke(
+        {"input_documents": docs, "question": user_question}
+    )
 
     print(response)
     st.write("Reply: ", response["output_text"])
@@ -81,9 +79,10 @@ def user_input(user_question):
 
 
 
+
 def main():
     st.set_page_config("Chat PDF")
-    st.header("Chat with PDF using Gemini💁")
+    st.header("ChatPDF Developed by Shalom Tehe")
 
     user_question = st.text_input("Ask a Question from the PDF Files")
 
@@ -104,3 +103,5 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
